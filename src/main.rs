d0df34530a6fe4fe9ee1e59c4af3d724b038b98f
João Vitor Yukio Bordin Yamashita <yukiobr98@gmail.com>
2023-05-31 20:51:55 -0300

    Add variables example

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
 fn main() {
     let res = print_welcome(&"Player 1");
     println!("{}", res);
+    create_variables();
+
+    let sum = add(10, 20);
+    println!("Sum: {}", sum);
 }
 
 fn print_welcome(name: &str) -> &str{
     println!("Welcome to the Snake Game, {}!", &name);
     return "Welcome to the Snake Game!";
+}
+
+fn create_variables()
+{
+    let tuple = (20, "Ola", 10);        
+    println!("Tuple: {:?}", tuple);
+
+    let x = [1,2,3];
+
+    println!("Array: {}", x[2]);
+
+    let y = [2; 10];
+
+    println!("Array: {:?}", y);
+}   
+
+fn add(x: u32, y:u32)->u32{
+    let sum = x+y;
+    return sum;
 }
